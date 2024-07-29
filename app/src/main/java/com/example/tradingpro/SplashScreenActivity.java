de6a522a662c951091d34836c333d32accdbc035
Tushar5201 <tusharlakadiya@gmail.com>
2024-07-29 23:22:44 +0530

    Initial commit

---
+++ app/src/main/java/com/example/tradingpro/SplashScreenActivity.java
@@ -0,0 +1,62 @@
+package com.example.tradingpro;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    ImageView imgLogo;
+    TextView tvAppname;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash_screen);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Animation zoom;
+        final Animation[] fade = new Animation[1];
+        imgLogo = findViewById(R.id.imgLogo);
+        tvAppname = findViewById(R.id.tvAppname);
+
+        zoom = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.zoom_anim);
+        imgLogo.startAnimation(zoom);
+
+        fade[0] = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_anim);
+        tvAppname.startAnimation(fade[0]);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                tvAppname.setText("...");
+                fade[0] = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_anim);
+                tvAppname.startAnimation(fade[0]);
+            }
+        }, 2500);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(SplashScreenActivity.this, LoginActivity.class);
+                startActivity(mainIntent);
+            }
+        }, 4200);
+
+    }
+}
