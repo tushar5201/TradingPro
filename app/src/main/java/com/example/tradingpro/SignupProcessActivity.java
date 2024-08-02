8f6ec4a3156f3b90d6c654f4cb38ff45da95f485
Tushar5201 <tusharlakadiya@gmail.com>
2024-08-02 15:32:54 +0530

    stepview improved

--- app/src/main/java/com/example/tradingpro/SignupProcessActivity.java
+++ app/src/main/java/com/example/tradingpro/SignupProcessActivity.java
@@ -1,18 +1,38 @@
 package com.example.tradingpro;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.shuhart.stepview.StepView;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class SignupProcessActivity extends AppCompatActivity {
 
-    StepView step_view;
+    StepView stepView;
+    FragmentManager fragmentManager;
+    public int currentStep = 0;
+    Button btnContinue;
+
+    List<Fragment> fragments = Arrays.asList(
+            new PersonalInformationFragment(),
+            new IdInformationFragment(),
+            new MpinFragment(),
+            new BiometricsEnableFragment(),
+            new DisclaimerFragment()
+    );
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,7 +44,36 @@ public class SignupProcessActivity extends AppCompatActivity {
             return insets;
         });
 
-        step_view = findViewById(R.id.step_view);
-        step_view.setStepsNumber(4);
+        stepView = findViewById(R.id.step_view);
+        btnContinue = findViewById(R.id.btnContinue);
+        fragmentManager = getSupportFragmentManager();
+
+        stepView.setStepsNumber(fragments.size());
+        loadFragment(currentStep);
+
+        btnContinue.setOnClickListener(v -> {
+            if (currentStep < fragments.size() - 1) {
+                currentStep++;
+                stepView.go(currentStep, true);
+                loadFragment(currentStep);
+            } else {
+                Toast.makeText(this, "Completed.", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void loadFragment(int step) {
+        Fragment fragment = fragments.get(step);
+        fragmentManager.beginTransaction().replace(R.id.fragment_container, fragment).commit();
+    }
+
+    public void loadFragment(Fragment fragment) {
+// create a FragmentManager
+
+// create a FragmentTransaction to begin the transaction and replace the Fragment
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+// replace the FrameLayout with new Fragment
+        fragmentTransaction.replace(R.id.fragment_container, fragment);
+        fragmentTransaction.commit(); // save the changes
     }
 }
