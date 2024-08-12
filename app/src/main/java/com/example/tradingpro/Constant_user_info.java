f9474c074fc65d4448bdcfc335f75c068e00f10f
vidhanshu37 <kelawalavidhanshu@gmail.com>
2024-08-12 16:43:07 +0530

    sihnup

---
+++ app/src/main/java/com/example/tradingpro/Constant_user_info.java
@@ -0,0 +1,9 @@
+package com.example.tradingpro;
+
+public class Constant_user_info {
+    public static final String TABLE_NAME = "user_info";
+    public static final String  KEY_USERNAME= "username";
+    public static final String KEY_EMAIL = "email";
+    public static final String KEY_PASSWORD = "password";
+    public static final String KEY_PHONE = "phone";
+}
