a492b64b4456c5673c42475cd9b8ec2f57a77a6e
vidhanshu37 <kelawalavidhanshu@gmail.com>
2024-08-17 14:06:49 +0530

    motion splash

--- app/src/main/java/com/example/tradingpro/LoginActivity.java
+++ app/src/main/java/com/example/tradingpro/LoginActivity.java
@@ -4,10 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
+import androidx.constraintlayout.motion.widget.MotionLayout;
+import androidx.constraintlayout.motion.widget.MotionScene;
+import androidx.constraintlayout.widget.ConstraintSet;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -22,6 +26,8 @@ public class LoginActivity extends AppCompatActivity {
     TextInputLayout textInputLayoutEmailOrPhnLog, textInputLayoutPasswordLog;
     TextInputEditText textInputEdEmailOrPhnLog, textInputEdPasswordLog;
     String emailOrPhone, password;
+//    boolean isCheckSplash = true;
+    MotionLayout main;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,14 +42,26 @@ public class LoginActivity extends AppCompatActivity {
         textInputLayoutPasswordLog = findViewById(R.id.textInputLayoutPasswordLog);
         textInputEdEmailOrPhnLog = findViewById(R.id.textInputEdEmailOrPhnLog);
         textInputEdPasswordLog = findViewById(R.id.textInputEdPasswordLog);
+        main = findViewById(R.id.main);
+
+//        main.setTransition(R.id.transition_splash);
+//        main.setProgress(1.0f);
+
+        if(!Constant_user_info.isCheckSplash) {
+            MotionScene.Transition transition = main.getTransition(R.id.transition_splash);
+            transition.setAutoTransition(MotionScene.Transition.AUTO_JUMP_TO_END);
+        } else {
+            MotionScene.Transition transition = main.getTransition(R.id.transition_splash);
+            transition.setAutoTransition(MotionScene.Transition.AUTO_ANIMATE_TO_END);
+        }
 
         btnLogin.setOnClickListener(v-> {
             emailOrPhone = textInputEdEmailOrPhnLog.getText().toString().trim();
             password = textInputEdPasswordLog.getText().toString().trim();
-
         });
 
         btnsignup.setOnClickListener(v -> {
+            Constant_user_info.isCheckSplash = false;
             Intent intent = new Intent(LoginActivity.this,SignupActivity.class);
             startActivity(intent);
         });
