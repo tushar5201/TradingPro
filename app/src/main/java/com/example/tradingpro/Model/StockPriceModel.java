f196f5e6eb392ff90615e5a4647242625c0aacb1
Tushar5201 <tusharlakadiya@gmail.com>
2024-09-11 10:57:20 +0530

    line chart & Login, mpin glitch solved

--- app/src/main/java/com/example/tradingpro/Model/StockPriceModel.java
+++ app/src/main/java/com/example/tradingpro/Model/StockPriceModel.java
@@ -1,5 +1,11 @@
 package com.example.tradingpro.Model;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+import io.finnhub.api.models.Indicator;
+
 public class StockPriceModel {
     public Chart chart;
 
@@ -8,6 +14,8 @@ public class StockPriceModel {
 
         public static class Result {
             public Meta meta;
+            public ArrayList<Integer> timestamp;
+            public Indicators indicators;
         }
     }
 
@@ -15,4 +23,11 @@ public class StockPriceModel {
         public String regularMarketPrice, previousClose, fiftyTwoWeekHigh, fiftyTwoWeekLow, regularMarketDayHigh, regularMarketDayLow, regularMarketVolume;
     }
 
+    public static class Indicators {
+        public Quote[] quote;
+
+        public static class Quote {
+            public ArrayList<Float> close;
+        }
+    }
 }
